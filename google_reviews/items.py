4f2559bdec8e4ddf4b28e3632e4cfa9b4a49c0f9
Meriem-s <saidmeriem96@gmail.com>
2023-01-29 23:02:09 +0100

    fieldnames update

--- google_reviews/items.py
+++ google_reviews/items.py
@@ -4,26 +4,24 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import datetime
-import scrapy
+from dataclasses import dataclass
 
 
-class GoogleReviewItem(scrapy.Item):
-    name = scrapy.Field()
-    content = scrapy.Field()
-    link = scrapy.Field()
-    rating = scrapy.Field()
-    time = scrapy.Field()
-    did_owner_reply = scrapy.Field()
-    owner_reply = scrapy.Field()
+@dataclass
+class GoogleReviewItem:
+    name: str
+    content: str
+    link: str
+    rating: int
+    time: str
+    did_owner_reply: bool
+    owner_reply: str
 
     def __init__(self, review_raw: dict):
-        owner_reply = review_raw[9][1] if review_raw[9] else ""
-        super(GoogleReviewItem, self).update(
-            {"name": review_raw[0][1],
-             "content": review_raw[3],
-             "link": review_raw[18],
-             "rating": review_raw[4],
-             "time": datetime.datetime.utcfromtimestamp(review_raw[27] / 1000),
-             "did_owner_reply": review_raw[9],
-             "owner_reply":   owner_reply     
-            })
+        self.name = review_raw[0][1]
+        self.content = review_raw[3]
+        self.link = review_raw[18]
+        self.rating = review_raw[4]
+        self.time = (datetime.datetime.utcfromtimestamp(review_raw[27] / 1000),)
+        self.did_owner_reply = review_raw[9] is not None
+        self.owner_reply = review_raw[9][1] if self.did_owner_reply else ""
