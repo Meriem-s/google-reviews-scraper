f57bc56ebec34262ce5d6c58ef553fc566df3a81
Meriem-s <saidmeriem96@gmail.com>
2023-02-02 01:15:49 +0100

    delete unused tests

--- test/test_output.py
+++
@@ -1,2 +0,0 @@
-# test number of reviews obtained in the csv file is equal  to the number of the reviews in this page
-# https://www.google.com/maps/place/Aspria+Berlin+Ku%E2%80%99damm/@52.5003887,13.2941771,15z/data=!4m11!3m10!1s0x0:0x2faf0f02eacd864e!5m3!1s2023-06-17!4m1!1i2!8m2!3d52.5003887!4d13.2941771!9m1!1b1
