0a3bf8b972822f4048421e3f10f591732db92aa9
Meriem-s <saidmeriem96@gmail.com>
2023-02-02 01:15:09 +0100

    move to new class

--- cli.py
+++ cli.py
@@ -3,24 +3,33 @@ import argparse
 import scrapy.crawler
 
 from google_reviews.spiders.googlereviewspider import GoogleReviewsSpider
-from log.logger import logger
+from google_reviews.utils import count_rows_csv
+
 
 # e.g. args.search "Neni Berlin"
+class GoogleReviewsCrawler:
+    def __init__(self, search: str, output: str = ""):
+        self.search = search
+        self.output = output
 
-parser = argparse.ArgumentParser()
-parser.add_argument("search", help="search for the business")
-parser.add_argument("-o", "--output", help="the filename of the csv output file")
-args = parser.parse_args()
+    def run(self):
+        if not self.search:
+            raise "please enter a correct search venue"
+        process = scrapy.crawler.CrawlerProcess()
+        if self.output:
+            process.crawl(
+                GoogleReviewsSpider, search_venue=self.search, csv_filename=self.output
+            )
+        else:
+            process.crawl(GoogleReviewsSpider, search_venue=self.search)
+        process.start()
+        count_rows_csv("reviews.csv")
 
 
 if __name__ == "__main__":
-    if not args.search:
-        raise "please enter a correct search venue"
-    process = scrapy.crawler.CrawlerProcess()
-    if args.output:
-        process.crawl(
-            GoogleReviewsSpider, search_venue=args.search, csv_filename=args.output
-        )
-    else:
-        process.crawl(GoogleReviewsSpider, search_venue=args.search)
-    process.start()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("search", help="search for the business")
+    parser.add_argument("-o", "--output", help="the filename of the csv output file")
+    args = parser.parse_args()
+    crawler = GoogleReviewsCrawler(search=args.search, output=args.output)
+    crawler.run()
